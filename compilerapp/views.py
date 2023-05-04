634b41b6dbe7f4fb1636624a08b558dfe22a5558
AnthonyGrod <tantien02@hotmail.com>
2023-05-05 00:02:51 +0200

    Basic dynamic catalog view added. TODO: nesting.

--- compilerapp/views.py
+++ compilerapp/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Catalog, File, Section
+
 def index(request):
-    return render(request, 'compilerapp/index.html')
+    catalogs_list = Catalog.objects.order_by('-created_at')[:]
+    template = loader.get_template('compilerapp/index.html')
+    context = {
+        'catalogs_list': catalogs_list,
+    }
+    return HttpResponse(template.render(context, request))
